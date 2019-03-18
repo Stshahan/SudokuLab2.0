3725905023085773f48460e9012aa18db013f68f
stsha <stsha@10.0.0.193>
2019-03-18 15:53:46 -0400

    PuzzleViolation test complete.

--- SudokuBLL/src/main/java/pkgHelper/PuzzleViolation.java
+++ SudokuBLL/src/main/java/pkgHelper/PuzzleViolation.java
@@ -6,17 +6,18 @@ public class PuzzleViolation {
 	private ePuzzleViolation ePuzzleViolation;
 	private int iValue;
 	
-	
+	//Constructor for PuzzleViolation
 	public PuzzleViolation (pkgEnum.ePuzzleViolation ePuzzleViolation,int iValue) {
 		super();
 		this.ePuzzleViolation = ePuzzleViolation;
 		this.iValue = iValue;
 		
 	}
+	//getter for ePuzzleViolation
 	public ePuzzleViolation getePuzzleViolation() {
 		return ePuzzleViolation;
 	}
-	
+	//getter for iValue
 	public int getiValue() {
 		return iValue;
 	}

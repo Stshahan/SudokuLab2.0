842cc29192b1e9598a5bd9cf496d6cce7601e09d
stsha <stsha@10.0.0.193>
2019-03-18 21:15:45 -0400

    getRegion issue

--- SudokuBLL/src/test/java/pkgGame/SudokuTest.java
+++ SudokuBLL/src/test/java/pkgGame/SudokuTest.java
@@ -1,4 +1,9 @@
 package pkgGame;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import pkgEnum.ePuzzleViolation;
+import org.apache.commons.lang.ArrayUtils;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +21,17 @@ public void TestRegionNbr() throws Exception {
 	Sudoku samplePuzzle = new Sudoku(Puz1);
 	
 	int[] regionNbr = {3,4,4,1};
-	
+	/*
+	 * {0,1,2,3},
+	 * {1,2,3,4},
+	 * {3,4,1,2},
+	 * {4,1,3,2};
+	 * 	 */
 	//Shaun can you look at this?????
 	
-	System.out.println(samplePuzzle.getRegion(2));
-	System.out.println(samplePuzzle.getRegion(1,2));
+	System.out.println(Arrays.toString(samplePuzzle.getRegion(2)));
+	System.out.println(Arrays.toString(samplePuzzle.getRegion(1,2)));
+	
 	//assertEquals(regionNbr, samplePuzzle.getRegion(1,2));
 	
 }
